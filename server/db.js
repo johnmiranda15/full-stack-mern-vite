92513a3c55614af3804b0da24acbb7cdc727efc1
Juan De Dios <mirandajohn634@gmail.com>
2024-12-27 15:04:40 -0500

    se mejora el codigo de conexion a la base de datos

--- server/db.js
+++ server/db.js
@@ -4,11 +4,21 @@ import { MONGODB_URI } from "./config.js";
 export const connectDB = async () => {
   try {
     await mongoose.connect(MONGODB_URI);
+    console.log("MongoDB connected successfully");
   } catch (error) {
-    console.error(error);
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1);
   }
 };
 
 mongoose.connection.on("connected", () => {
-  console.log("Mongodb is connected to", mongoose.connection.db.databaseName);
+  console.log("Mongoose connected to", mongoose.connection.db.databaseName);
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error("Mongoose connection error:", err);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongoose disconnected");
 });
