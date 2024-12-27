1dac28895cc3a61027977c34c6284e8b715f497d
Juan De Dios <mirandajohn634@gmail.com>
2024-12-27 10:04:50 -0500

    se agregan nuevas constantes de variables de entorno

--- server/config.js
+++ server/config.js
@@ -1,8 +1,13 @@
 import { config } from "dotenv";
 config();
 
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/merndb";
+export const MONGODB_URI = process.env.MONGODB_URI;
 export const PORT = process.env.PORT || 4000;
 export const CLOUD_NAME = process.env.CLOUD_NAME;
 export const API_KEY = process.env.API_KEY;
 export const API_SECRET = process.env.API_SECRET;
+export const DELETE_KEY = process.env.DELETE_KEY; 
+export const SECRET = process.env.SECRET;
+export const ADMIN_EMAIL = process.env.ADMIN_EMAIL;
+export const ADMIN_USERNAME = process.env.ADMIN_USERNAME;
+export const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
