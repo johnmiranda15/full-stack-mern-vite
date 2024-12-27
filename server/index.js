479af1d5feda17107e6b4cb23080814fc9018efd
Juan De Dios <mirandajohn634@gmail.com>
2024-12-27 15:39:50 -0500

    update index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import { app } from "./app.js";
 import { connectDB } from "./db.js";
 import { PORT } from "./config.js";
-import "./libs/initialSetup.js";
+import { initializeDatabase } from "./libs/initialSetup.js";
 
 connectDB();
+// Inicialización de base de datos
+initializeDatabase();
 app.listen(PORT);
 console.log("Server on port", PORT);
