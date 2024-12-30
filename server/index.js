71c03b6d9e67df66a850e0362666d250c3861967
Juan De Dios <mirandajohn634@gmail.com>
2024-12-30 08:57:43 -0500

    se elimina ejecución de funcion middleware y se pasa a db,js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,7 @@
 import { app } from "./app.js";
 import { connectDB } from "./db.js";
 import { PORT } from "./config.js";
-import { initializeDatabase } from "./libs/initialSetup.js";
 
 connectDB();
-// Inicialización de base de datos
-initializeDatabase();
 app.listen(PORT);
 console.log("Server on port", PORT);
