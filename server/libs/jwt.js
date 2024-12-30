af1420e5bac24a19eb0a74fea834c4e4c455c2de
Juan De Dios <mirandajohn634@gmail.com>
2024-12-30 09:02:08 -0500

    se crea funcionalidad de creacion del token

---
+++ server/libs/jwt.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+import { TOKEN_SECRET } from "../config.js";
+
+export function createAccessToken(payload) {
+  return new Promise((resolve, reject) => {
+    jwt.sign(
+      payload,
+      TOKEN_SECRET,
+      {
+        expiresIn: "1d",
+      },
+      (error, token) => {
+        if (error) {
+          console.error(error);
+          reject("No se pudo generar el token");
+        }
+        resolve(token);
+      }
+    );
+  });
+}
